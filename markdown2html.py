05d6643daf32f6e22918f8afce4a7f8de58c0948
Aymenbs22 <aymenbenslima1@gmail.com>
2023-03-30 21:12:24 +0100

    script markdown2html.py that takes an argument 2 strings:
    First argument is the name of the Markdown file
    Second argument is the output file name

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''script markdown2html.py that takes an argument 2 strings:
+"""script markdown2html.py that takes an argument 2 strings:
 First argument is the name of the Markdown file
-Second argument is the output file name'''
+Second argument is the output file name"""
 
 import markdown
 import sys
