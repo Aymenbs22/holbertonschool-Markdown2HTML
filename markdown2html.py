637f88b64c536bbdaebff9b62eee6c69e112f5cb
Aymenbs22 <aymenbenslima1@gmail.com>
2023-03-30 21:41:37 +0100

    script markdown2html.py that takes an argument 2 strings:
    First argument is the name of the Markdown file
    Second argument is the output file name

--- markdown2html.py
+++ markdown2html.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """script markdown2html.py that takes an argument 2 strings"""
-
 import markdown
 import sys
 
+
+"""script markdown2html.py that takes an argument 2 strings"""
 if len(sys.argv) != 3:
     print("Usage: ./markdown2html.py README.md README.html")
     sys.exit(1)
